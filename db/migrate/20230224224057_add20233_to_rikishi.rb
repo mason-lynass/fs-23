2f25f2038f5f3240d1e738d630241044d6825bd6
Mason <masonlynass@gmail.com>
2023-02-24 20:20:57 -0800

    think i got it this time

--- db/migrate/20230224224057_add20233_to_rikishi.rb
+++
@@ -1,7 +0,0 @@
-class Add20233ToRikishi < ActiveRecord::Migration[7.0]
-  def change
-    # add_column :rikishis, :FS_20233, :integer
-    remove_column :rikishis, :FS_20226
-    remove_column :rikishis, :FS_20231
-  end
-end
