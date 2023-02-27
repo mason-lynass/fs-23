b6d00ef1e98459f7f3d6aa02f8952bb5c436202a
Mason <masonlynass@gmail.com>
2023-02-27 10:35:59 -0800

    changed OneTeam

--- client/src/components/OneTeam.js
+++ client/src/components/OneTeam.js
@@ -1,35 +1,38 @@
 function OneTeam({ team }) {
     // console.log(team)
+
+    // change it back to FS_history[13] once you push the scores to their arrays
+
     return (
         <div className='oneTeam'>
             <h2>{team.user.username}</h2>
             <div className="smallerColumn">
                 <p>{team[0].shikona}</p>
-                <p>{team[0].FS_history[12]}</p>
+                <p>{team[0].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[1].shikona}</p>
-                <p>{team[1].FS_history[12]}</p>
+                <p>{team[1].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[2].shikona}</p>
-                <p>{team[2].FS_history[12]}</p>
+                <p>{team[2].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[3].shikona}</p>
-                <p>{team[3].FS_history[12]}</p>
+                <p>{team[3].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[4].shikona}</p>
-                <p>{team[4].FS_history[12]}</p>
+                <p>{team[4].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[5].shikona}</p>
-                <p>{team[5].FS_history[12]}</p>
+                <p>{team[5].FS_20233}</p>
             </div>
             <div className="smallerColumn">
                 <p>{team[6].shikona}</p>
-                <p>{team[6].FS_history[12]}</p>
+                <p>{team[6].FS_20233}</p>
             </div>
             <h3 className="total">{team.scoreSum}</h3>
         </div>
