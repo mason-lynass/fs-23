ed74bc6be8dbeb386858133019838c4d6bae7ddf
Mason <masonlynass@gmail.com>
2023-02-28 20:34:59 -0800

    draft mobile css changes

--- client/src/components/SmallCard.js
+++ client/src/components/SmallCard.js
@@ -2,7 +2,7 @@ function SmallCard ({rikishi, handleCardClick}) {
     return (
         <div id={rikishi.current_rank} className="SmallCard" onClick={() => handleCardClick(rikishi)}>
             <img className="smallImage" src={rikishi.image_url} alt={rikishi.shikona}></img>
-            <h3>{rikishi.shikona}</h3>
+            <h3 className="draftShikona">{rikishi.shikona}</h3>
             <p>{rikishi.current_rank}</p>
         </div>
     )
