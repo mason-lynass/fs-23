36f6f7e6b44719d0792ba4214d4756ddbb6bc7d1
Mason <masonlynass@gmail.com>
2023-03-03 11:25:08 -0800

    no bad words first test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,28 @@
 class User < ApplicationRecord
+
     has_many :teams
 
     has_secure_password
 
     validates :username, uniqueness: true, presence: true
+    validate :no_bad_words, :no_spaces
+
+    private
+
+    BAD_WORDS = %w(nigg chink cunt bitch shit cock penis vagina pussy dick balls fuck nazi skinhead kike faggot rape jew)
+
+    def no_spaces
+        if username.include?(" ")
+            errors.add(:username, "no spaces please")
+        end
+    end
+
+    def no_bad_words
+        justLetters = username.gsub(/[^a-zA-Z]/, '')
+        lowerName = justLetters.downcase
+        if BAD_WORDS.any? { |word| lowerName.include?(word)}
+            errors.add(:username, "please stop")
+        end
+    end
+
 end
