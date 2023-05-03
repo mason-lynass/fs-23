46f94126139aa90b4c9a20956ac2e3d5c1e1b6e0
Mason <masonlynass@gmail.com>
2023-05-03 16:53:40 -0700

    ichinojo retired!

--- client/src/Draft.js
+++ client/src/Draft.js
@@ -132,7 +132,7 @@ function Draft({ user, setUser, rikishi, tachiai, clap, rankSort, fsHistories })
 
     // this is where you filter out rikishi if anyone is injured or absent before the tournament
     // const MakuuchiRikishi = MRikishi.filter(rikishi => rikishi.current_rank !== "J" && rikishi.shikona !== "Ichinojo" && rikishi.shikona !== "Terunofuji")
-    const MakuuchiRikishi = MRikishi.filter(rikishi => rikishi.current_rank !== "J" && rikishi.current_rank !== "MS" && rikishi.shikona !== "Wakatakakage")
+    const MakuuchiRikishi = MRikishi.filter(rikishi => rikishi.current_rank !== "J" && rikishi.current_rank !== "MS" && rikishi.shikona !== "Wakatakakage" && rikishi.shikona !== "Ichinojo")
     const sortedMRikishi = rankSort(MakuuchiRikishi, null)
     const JuryoRikishi = draftRikishi.filter(rikishi => rikishi.current_rank === "J")
 
@@ -168,7 +168,7 @@ function Draft({ user, setUser, rikishi, tachiai, clap, rankSort, fsHistories })
                                 <option value="13">M13+ only</option>
                             </select>
                         </div>
-                        <p id="headsup">Wakatakakage will not be competing in this tournament</p>
+                        <p id="headsup">Wakatakakage will not be competing in this tournament. Ichinojo has retired from professional sumo!</p>
                     </div>
                 </div>
                 <p id='clickText'>click on a wrestler to display info and add them to your team!</p>
