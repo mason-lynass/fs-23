64fc6b0dcc8f3230afc4f404304690a3845c6593
Mason <masonlynass@gmail.com>
2023-05-14 11:16:01 -0700

    fixing rikishi results

--- client/src/components/JResults.js
+++ client/src/components/JResults.js
@@ -5,7 +5,7 @@ function JResults ({JRikishi}) {
             return (
                 <div className="oneRR" id={r.shikona} key={r.shikona}>
                     <h2 className="RRShikona">{r.shikona}</h2>
-                    <h2 className="RRPoints">{r.FS_20233}</h2>
+                    <h2 className="RRPoints">{r.fs_current}</h2>
                 </div>
             )
         })
