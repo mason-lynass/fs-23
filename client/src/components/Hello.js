54086a9e7d3329b8fbbda2d692610e11722d721e
Mason <masonlynass@gmail.com>
2023-07-08 23:50:14 -0700

    whoops were live

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -1,8 +1,8 @@
 function Hello() {
     return (
         <div id="hello">
-             <p> The draft is open! The next tournament begins on July 9th, so make sure you draft before then!</p>
-             {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the July tournament.</p> */}
+             {/* <p> The draft is open! The next tournament begins on July 9th, so make sure you draft before then!</p> */}
+             <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the September tournament.</p>
             {/* <p>Congrats to Pmwilkins & chung for your Fantasy Sumo victories! </p><p> The new rankings will be announced on June 26th, so the draft will be open from June 27th - July 8th.</p> */}
         </div>
     )
