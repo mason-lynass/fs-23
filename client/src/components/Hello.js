3ddf5d7ab1c041a47ffeed4dd76a986a1373d408
Mason <masonlynass@gmail.com>
2023-07-08 23:58:38 -0700

    for real

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -3,7 +3,7 @@ function Hello() {
         <div id="hello">
              {/* <p> The draft is open! The next tournament begins on July 9th, so make sure you draft before then!</p> */}
              <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the September tournament.</p>
-             <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p>
+             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
             {/* <p>Congrats to Pmwilkins & chung for your Fantasy Sumo victories! </p><p> The new rankings will be announced on June 26th, so the draft will be open from June 27th - July 8th.</p> */}
         </div>
     )
