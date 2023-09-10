c1f35a7d990383e0fd8cca16acbcd67c6c3cd7e1
Mason <masonlynass@gmail.com>
2023-09-09 19:24:16 -0700

    final teams

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -2,10 +2,10 @@ function Hello() {
     return (
         <div id="hello">
             {/*  when it's time to draft */}
-            <p> The draft is open!<br/><br/> The next tournament begins on September 10th, so make sure you draft before then!</p>
+            {/* <p> The draft is open!<br/><br/> The next tournament begins on September 10th, so make sure you draft before then!</p> */}
 
             {/* when the tournament is live */}
-            {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the November tournament.</p> */}
+            <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the November tournament.</p>
 
             {/* if you want to let someone contact you for some reason */}
             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
