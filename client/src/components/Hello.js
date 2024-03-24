3c19cea0c876816ea80fb227c02bf19f8c5e89ef
Mason Lynass <109309370+mason-lynass@users.noreply.github.com>
2024-03-24 12:10:05 -0700

    Update Hello.js

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -5,13 +5,13 @@ function Hello() {
             {/* <p> The draft is open!<br/><br/> The next tournament begins on March 10th, so make sure you draft before then!</p> */}
 
             {/* when the tournament is live */}
-            <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p>
+            {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p> */}
 
             {/* if you want to let someone contact you for some reason */}
             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
 
             {/* after the tournament */}
-            {/* <p>Congrats to Itchynotoe on your Fantasy Sumo victory! </p><p> The new rankings will be announced on February 26th, so the draft will be open from February 27th - March 10th.</p> */}
+            <p>Congrats to Hisui, texaskevin, and KappyKaiju3 on your Fantasy Sumo victories! A three-way tie with three different teams, how exciting! </p><p> The new rankings will be announced on April 30th, so the draft will be open from May 1st - May 12th.</p><p>I'll update the database in a day or two. Thanks for playing everyone!</p>
         </div>
     )
 }
