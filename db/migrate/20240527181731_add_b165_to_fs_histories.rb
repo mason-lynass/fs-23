abe05a14466fd94724f9b78ad57a4fffe8aa2df7
Mason <masonlynass@gmail.com>
2024-05-27 11:24:06 -0700

    whoops wrong column name

--- db/migrate/20240527181731_add_b165_to_fs_histories.rb
+++ db/migrate/20240527181731_add_b165_to_fs_histories.rb
@@ -1,5 +1,5 @@
 class AddB165ToFsHistories < ActiveRecord::Migration[7.0]
   def change
-        add_column :fs_histories, :b164, :integer
+        add_column :fs_histories, :b165, :integer
   end
 end
