63b73600cb1da180f2ce8d68f41f57e9e316a7dd
Mason <masonlynass@gmail.com>
2024-05-30 13:46:29 -0700

    ugh

--- client/src/TeamRankings.js
+++ client/src/TeamRankings.js
@@ -11,67 +11,80 @@ function TeamRankings({ teams, teamsLoaded }) {
   // sort users by average rank, display the number of bashos, display their % of highest score in a table
 
   const users = [];
-  const bashos = []
+  const bashos = [];
 
-  const [bashosLoaded, setBashosLoaded] = useState(false)
-  const [usersLoaded, setUsersLoaded] = useState(false)
+  const [bashosLoaded, setBashosLoaded] = useState(false);
+  const [usersLoaded, setUsersLoaded] = useState(false);
 
   useEffect(() => {
     if (teamsLoaded === true) {
       for (let i = 0; i < teams.length; i++) {
         if (bashos.length === 0) {
-            bashos.push({basho: teams[i].basho, teams: [teams[i]]})
-        }
-        else if (bashos.length > 0 && (bashos.some((b) => b.basho === teams[i].basho) === false)) {
-            bashos.push({basho: teams[i].basho, teams: [teams[i]]})
-        }
-        else {
-            const target = bashos.find((b) => b.basho === teams[i].basho)
-            target.teams.push(teams[i])
+          bashos.push({ basho: teams[i].basho, teams: [teams[i]] });
+        } else if (
+          bashos.length > 0 &&
+          bashos.some((b) => b.basho === teams[i].basho) === false
+        ) {
+          bashos.push({ basho: teams[i].basho, teams: [teams[i]] });
+        } else {
+          const target = bashos.find((b) => b.basho === teams[i].basho);
+          target.teams.push(teams[i]);
         }
 
-        if (users.length > 0 && users.some((u) =>  u.username === teams[i].user.username)) continue;
+        if (
+          users.length > 0 &&
+          users.some((u) => u.username === teams[i].user.username)
+        )
+          continue;
         else {
-            users.push({username: teams[i].user.username})
+          users.push({ username: teams[i].user.username });
         }
       }
-      console.log(teams, bashos)
-      setBashosLoaded(true)
-      bashosCleanup()
+      console.log(teams, bashos);
+      setBashosLoaded(true);
+      bashosCleanup();
     }
   }, [teamsLoaded]);
 
-  function usersCleanup () {
+  function usersCleanup() {
     users.forEach((u) => {
-        const allPercentileKeys = Object.keys(u).filter((k) => k.includes('score')) //find the keys that have "score" in their name
-        const allPercentiles = allPercentileKeys.map(key => u[key])
-        const total = allPercentiles.reduce((acc, value) => parseFloat(acc) + parseFloat(value), 0)
-        console.log(total)
-        const average = (total / allPercentiles.length).toFixed(2) // something like this
-        u.average_percentile = average
-    })
-    console.log(users)
-    setUsersLoaded(true)
+      const allPercentileKeys = Object.keys(u).filter((k) =>
+        k.includes("score")
+      ); //find the keys that have "score" in their name
+      const allPercentiles = allPercentileKeys.map((key) => u[key]);
+      const total = allPercentiles.reduce(
+        (acc, value) => parseFloat(acc) + parseFloat(value),
+        0
+      );
+      console.log(total);
+      const average = (total / allPercentiles.length).toFixed(2); // something like this
+      u.average_percentile = average;
+    });
+    console.log(users);
+    setUsersLoaded(true);
   }
 
-  function bashosCleanup () {
+  function bashosCleanup() {
     bashos.forEach((b) => {
-        b.teams.sort((a,b) => b.final_score - a.final_score)
-        const highest_score = b.teams[0].final_score
-        b.teams.forEach((t) => {
-            const percentile = (t.final_score / highest_score).toFixed(2)
-            t.percentile = percentile
-            const targetUser = users.find((u) => u.username === t.user.username)
-            targetUser[`score${t.basho}`] = percentile
-        })
-    }) 
-    console.log(bashos, users)
-    usersCleanup()
+      b.teams.sort((a, b) => b.final_score - a.final_score);
+      const highest_score = b.teams[0].final_score;
+      b.teams.forEach((t) => {
+        const percentile = (t.final_score / highest_score).toFixed(2);
+        t.percentile = percentile;
+        const targetUser = users.find((u) => u.username === t.user.username);
+        targetUser[`score${t.basho}`] = percentile;
+      });
+    });
+    console.log(bashos, users);
+    usersCleanup();
   }
 
   function allTeams() {
     if (usersLoaded === true) {
-        const sortedUsers = users.sort((a, b) => b.average_percentile - a.average_percentile)
+      const sortedUsers = users.sort(
+        (a, b) => parseFloat(b.average_percentile) - parseFloat(a.average_percentile)
+      );
+      console.log(sortedUsers)
       return sortedUsers.map((user) => {
         return (
           <div className="oneTeam" key={user.username}>
