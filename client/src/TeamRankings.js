ecbbbd993ab2f71672319f5d9c4e4fb3a8e8083a
Mason <masonlynass@gmail.com>
2024-06-08 23:55:19 -0700

    maybe this will fix the sort?

--- client/src/TeamRankings.js
+++ client/src/TeamRankings.js
@@ -3,12 +3,11 @@ import "./CSS/teamrankings.css";
 import { useEffect, useState } from "react";
 
 function TeamRankings({ teams, teamsLoaded }) {
-
   // get all of the users
   // they already have teams with percentile values, so we don't need to calculate that
   // users also already have total_percentiles, so we don't need to calculate that
   // what we need to calculate is the average_percentile, which is just total_percentile / number of teams
-  // 
+  //
 
   // get all of the teams - this comes from a prop
   // go through all of the teams and create user objects for each username
@@ -26,7 +25,7 @@ function TeamRankings({ teams, teamsLoaded }) {
     fetch("/users")
       .then((r) => r.json())
       .then((r) => setAllUsers(r));
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (allUsers.length > 0) {
@@ -52,45 +51,98 @@ function TeamRankings({ teams, teamsLoaded }) {
   }
 
   function usersCleanup() {
-    console.log(allUsers);
     allUsers.forEach((u) => {
-      const total = u.total_percentile
-      const average = (parseFloat(total) / u.teams.length).toFixed(2); // something like this
+      const total = u.total_percentile;
+      let average = 0;
+      let weightedAverage = 0;
+      if (total > 0) {
+        average = (parseFloat(total) / u.teams.length).toFixed(2); // something like this
+        weightedAverage = (
+          parseFloat(average) +
+          0.1 * (u.teams.length - 1)
+        ).toFixed(2);
+      }
       u.average_percentile = average;
-      u.weighted_average = (
-        parseFloat(average) +
-        0.1 * (u.teams.length - 1)
-      ).toFixed(2);
+      u.weighted_average = weightedAverage;
     });
-    console.log(allUsers);
     setUsersLoaded(true);
   }
 
   function allTeams() {
-    if (usersLoaded === true && allUsers[allUsers.length - 1].average_percentile) {
+    if (
+      usersLoaded === true &&
+      allUsers[allUsers.length - 1].average_percentile
+    ) {
       console.log(allUsers);
-      let sortedUsers
-      if (sortState === 'default') {
+      let sortedUsers;
+      if (sortState === "default") {
         sortedUsers = allUsers.sort(
-            (a, b) =>
-              //   parseFloat(b.average_percentile) - parseFloat(a.average_percentile)
-              parseFloat(b.weighted_average) - parseFloat(a.weighted_average)
-          );
-      }
-      else if (sortState === 'TRUsername') sortedUsers = allUsers.sort((a, b) => a.username - b.username);
-      else if (sortState === 'TRAverage') sortedUsers = allUsers.sort((a, b) => parseFloat(b.average_percentile) - parseFloat(a.average_percentile));
-      else if (sortState === 'TRTotal') sortedUsers = allUsers.sort((a, b) => parseFloat(b.total) - parseFloat(a.total));
-      else if (sortState === 'TRWeighted') sortedUsers = allUsers.sort((a, b) => parseFloat(b.weighted_average) - parseFloat(a.weighted_average));
-      else if (sortState === '202301') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.01`]) - parseFloat(a[`score2023.01`]));
-      else if (sortState === '202303') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.03`]) - parseFloat(a[`score2023.03`]))
-      else if (sortState === '202305') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.05`]) - parseFloat(a[`score2023.05`]))
-      else if (sortState === '202307') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.07`]) - parseFloat(a[`score2023.07`]))
-      else if (sortState === '202309') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.09`]) - parseFloat(a[`score2023.09`]))
-      else if (sortState === '202311') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2023.11`]) - parseFloat(a[`score2023.11`]))
-      else if (sortState === '202401') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2024.01`]) - parseFloat(a[`score2024.01`]))
-      else if (sortState === '202403') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2024.03`]) - parseFloat(a[`score2024.03`]))
-      else if (sortState === '202405') sortedUsers = allUsers.sort((a, b) => parseFloat(b[`score2024.05`]) - parseFloat(a[`score2024.05`]))
-      
+          (a, b) =>
+            //   parseFloat(b.average_percentile) - parseFloat(a.average_percentile)
+            parseFloat(b.weighted_average) - parseFloat(a.weighted_average)
+        );
+      } else if (sortState === "TRUsername")
+        sortedUsers = allUsers.sort((a, b) => a.username - b.username);
+      else if (sortState === "TRAverage")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b.average_percentile) - parseFloat(a.average_percentile)
+        );
+      else if (sortState === "TRTotal")
+        sortedUsers = allUsers.sort(
+          (a, b) => parseFloat(b.total) - parseFloat(a.total)
+        );
+      else if (sortState === "TRWeighted")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b.weighted_average) - parseFloat(a.weighted_average)
+        );
+      else if (sortState === "202301")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.01`]) - parseFloat(a[`score2023.01`])
+        );
+      else if (sortState === "202303")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.03`]) - parseFloat(a[`score2023.03`])
+        );
+      else if (sortState === "202305")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.05`]) - parseFloat(a[`score2023.05`])
+        );
+      else if (sortState === "202307")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.07`]) - parseFloat(a[`score2023.07`])
+        );
+      else if (sortState === "202309")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.09`]) - parseFloat(a[`score2023.09`])
+        );
+      else if (sortState === "202311")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2023.11`]) - parseFloat(a[`score2023.11`])
+        );
+      else if (sortState === "202401")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2024.01`]) - parseFloat(a[`score2024.01`])
+        );
+      else if (sortState === "202403")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2024.03`]) - parseFloat(a[`score2024.03`])
+        );
+      else if (sortState === "202405")
+        sortedUsers = allUsers.sort(
+          (a, b) =>
+            parseFloat(b[`score2024.05`]) - parseFloat(a[`score2024.05`])
+        );
+
       console.log(sortedUsers);
       return sortedUsers.map((user) => {
         let username = user.username;
@@ -101,15 +153,21 @@ function TeamRankings({ teams, teamsLoaded }) {
           username = username.slice(0, at);
         }
 
-        const hatsu2023 = user.teams.filter((t) => t.basho === 2023.01)[0]  || ''
-        const haru2023 = user.teams.filter((t) => t.basho === 2023.03)[0]  || ''
-        const natsu2023 = user.teams.filter((t) => t.basho === 2023.05)[0]  || ''
-        const nagoya2023 = user.teams.filter((t) => t.basho === 2023.07)[0]  || ''
-        const aki2023 = user.teams.filter((t) => t.basho === 2023.09)[0]  || ''
-        const kyushu2023 = user.teams.filter((t) => t.basho === 2023.11)[0]  || ''
-        const hatsu2024 = user.teams.filter((t) => t.basho === 2024.01)[0]  || ''
-        const haru2024 = user.teams.filter((t) => t.basho === 2024.03)[0]  || ''
-        const natsu2024 = user.teams.filter((t) => t.basho === 2024.05)[0]  || ''
+        const hatsu2023 =
+          user.teams.filter((t) => t.basho === 2023.01)[0] || "";
+        const haru2023 = user.teams.filter((t) => t.basho === 2023.03)[0] || "";
+        const natsu2023 =
+          user.teams.filter((t) => t.basho === 2023.05)[0] || "";
+        const nagoya2023 =
+          user.teams.filter((t) => t.basho === 2023.07)[0] || "";
+        const aki2023 = user.teams.filter((t) => t.basho === 2023.09)[0] || "";
+        const kyushu2023 =
+          user.teams.filter((t) => t.basho === 2023.11)[0] || "";
+        const hatsu2024 =
+          user.teams.filter((t) => t.basho === 2024.01)[0] || "";
+        const haru2024 = user.teams.filter((t) => t.basho === 2024.03)[0] || "";
+        const natsu2024 =
+          user.teams.filter((t) => t.basho === 2024.05)[0] || "";
 
         return (
           <div className="oneTeamTR" key={user.username}>
@@ -117,15 +175,15 @@ function TeamRankings({ teams, teamsLoaded }) {
             <h3 className="totalTR">{user.average_percentile}</h3>
             <h3 className="totalTR">{user.total_percentile}</h3>
             <h3 className="totalTR">{user.weighted_average}</h3>
-            <p>{hatsu2023 !== '' ? hatsu2023.percentile : ''}</p>
-            <p>{haru2023 !== '' ? haru2023.percentile : ''}</p>
-            <p>{natsu2023 !== '' ? natsu2023.percentile : ''}</p>
-            <p>{nagoya2023 !== '' ? nagoya2023.percentile : ''}</p>
-            <p>{aki2023 !== '' ? aki2023.percentile : ''}</p>
-            <p>{kyushu2023 !== '' ? kyushu2023.percentile : ''}</p>
-            <p>{hatsu2024 !== '' ? hatsu2024.percentile : ''}</p>
-            <p>{haru2024 !== '' ? haru2024.percentile : ''}</p>
-            <p>{natsu2024 !== '' ? natsu2024.percentile : ''}</p>
+            <p>{hatsu2023 !== "" ? hatsu2023.percentile : ""}</p>
+            <p>{haru2023 !== "" ? haru2023.percentile : ""}</p>
+            <p>{natsu2023 !== "" ? natsu2023.percentile : ""}</p>
+            <p>{nagoya2023 !== "" ? nagoya2023.percentile : ""}</p>
+            <p>{aki2023 !== "" ? aki2023.percentile : ""}</p>
+            <p>{kyushu2023 !== "" ? kyushu2023.percentile : ""}</p>
+            <p>{hatsu2024 !== "" ? hatsu2024.percentile : ""}</p>
+            <p>{haru2024 !== "" ? haru2024.percentile : ""}</p>
+            <p>{natsu2024 !== "" ? natsu2024.percentile : ""}</p>
           </div>
         );
       });
@@ -137,7 +195,9 @@ function TeamRankings({ teams, teamsLoaded }) {
   ) : (
     <div id="team-rankings-box">
       <div className="oneTeamTR" id="TRHeaderColumn">
-        <h2 className="TRCol" id='TRUsername'>username</h2>
+        <h2 className="TRCol" id="TRUsername">
+          username
+        </h2>
         <h3 className="totalTR TRCol" id="TRAverage" onClick={handleSortState}>
           average score
         </h3>
