18e8c0d95990c9eefd648503f94a57a07929508d
Mason Lynass <109309370+mason-lynass@users.noreply.github.com>
2024-07-13 23:38:18 -0700

    Update Draft.js

--- client/src/Draft.js
+++ client/src/Draft.js
@@ -292,15 +292,15 @@ function Draft({ user, setUser, rikishi, tachiai, clap, rankSort, fsHistories, b
     }
 
     // turn this back on when it's time to draft (it's not right now)
-    return (
-        (rikishiLoaded === true) ?
-            areYouLoggedIn()
-            : <p style={{ textAlign: 'center'}}>loading...</p>
-    )
+   // return (
+        //(rikishiLoaded === true) ?
+            //areYouLoggedIn()
+            //: <p style={{ textAlign: 'center'}}>loading...</p>
+    //)
 
-    // return (
-    //     <Hello />
-    // )
+     return (
+         <Hello />
+    )
 }
 
 export default Draft
