71b0dcaa0715201bbc52a1c3fe61776596679372
Mason <masonlynass@gmail.com>
2024-10-22 14:13:40 -0700

    pre-Kyushu draft on

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -2,7 +2,7 @@ function Hello() {
     return (
         <div id="hello">
             {/*  when it's time to draft */}
-            {/* <p> The draft is open!<br/><br/> The next tournament begins on September 8th, so make sure you draft before then!</p> */}
+            <p> The draft is open!<br/><br/> The next tournament begins on November 10th, so make sure you draft before then!</p>
 
             {/* when the tournament is live */}
             {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p>  */}
@@ -11,7 +11,7 @@ function Hello() {
             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
 
             {/* after the tournament */}
-            <p>Congrats to jegeest on your Fantasy Sumo victory!</p><p> The new rankings will be announced on October 28th, so the draft will be open from October 29th - November 10th.</p><p>Thanks for playing everyone!</p>
+            {/* <p>Congrats to jegeest on your Fantasy Sumo victory!</p><p> The new rankings will be announced on October 28th, so the draft will be open from October 29th - November 10th.</p><p>Thanks for playing everyone!</p> */}
         </div>
     )
 }
