05aabeee4ed39451d46ea9b2d2775fcb870cfdc6
Mason <masonlynass@gmail.com>
2025-01-24 22:52:02 -0800

    post hatsu

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -5,13 +5,13 @@ function Hello() {
             {/* <p> The draft is open!<br/><br/> The next tournament begins on January 12th, so make sure you draft before then!</p> */}
 
             {/* when the tournament is live */}
-            <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p> 
+            {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p>  */}
 
             {/* if you want to let someone contact you for some reason */}
             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
 
             {/* after the tournament */}
-            {/* <p>Congrats to HeyZee on your Fantasy Sumo victory!</p><p> The new rankings will be announced on December 23rd, so the draft will be open from December 24th - January 12th.</p><p>Thanks for playing everyone!</p> */}
+            {/* <p>Congrats to HeyZee on your Fantasy Sumo victory!</p><p> The new rankings will be announced on February 25th, so the draft will be open from February 26th - March 9th.</p><p>Thanks for playing everyone!</p> */}
         </div>
     )
 }
