188795236718e212bb604b5c0d6a6015d031110a
Mason Lynass <109309370+mason-lynass@users.noreply.github.com>
2025-01-26 07:04:13 -0800

    Update Hello.js

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -11,7 +11,7 @@ function Hello() {
             {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
 
             {/* after the tournament */}
-            {/* <p>Congrats to deathlyhalliwell on your Fantasy Sumo victory!</p><p> The new rankings will be announced on February 25th, so the draft will be open from February 26th - March 9th.</p><p>Thanks for playing everyone!</p> */}
+            <p>Congrats to deathlyhalliwell on your Fantasy Sumo victory!</p><p> The new rankings will be announced on February 25th, so the draft will be open from February 26th - March 9th.</p><p>Thanks for playing everyone!</p>
         </div>
     )
 }
