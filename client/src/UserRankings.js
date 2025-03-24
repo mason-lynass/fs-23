c6f21b6b29b0062eb7a37f694645baf9ba521dd0
Mason <masonlynass@gmail.com>
2025-03-23 21:54:49 -0700

    user rankings

--- client/src/UserRankings.js
+++ client/src/UserRankings.js
@@ -127,6 +127,7 @@ function UserRankings() {
           user.teams.filter((t) => t.basho === 2024.11)[0] || "";
         const hatsu2025 =
           user.teams.filter((t) => t.basho === 2025.01)[0] || "";
+        const haru2025 = user.teams.filter((t) => t.basho === 2025.03)[0] || "";
 
         return (
           <div className="oneTeamTR" key={user.username}>
@@ -134,6 +135,7 @@ function UserRankings() {
             <h3 className="totalTR">{user.average_percentile}</h3>
             <h3 className="totalTR">{user.total_percentile}</h3>
             <h3 className="totalTR">{user.weighted_average}</h3>
+            <p>{haru2025 !== "" ? haru2025.percentile : ""}</p>
             <p>{hatsu2025 !== "" ? hatsu2025.percentile : ""}</p>
             <p>{kyushu2024 !== "" ? kyushu2024.percentile : ""}</p>
             <p>{aki2024 !== "" ? aki2024.percentile : ""}</p>
@@ -179,6 +181,9 @@ function UserRankings() {
           >
             weighted average
           </h3>
+          <p className="TRCol" id="2025.03" onClick={handleSortState}>
+            2025.03
+          </p>
           <p className="TRCol" id="2025.01" onClick={handleSortState}>
             2025.01
           </p>
