217a1a70725dda5aca8278511257fb2e74f93dab
Mason Lynass <masonlynass@gmail.com>
2025-09-13 21:52:42 -0700

    draft off

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -2,7 +2,7 @@ function Hello() {
   return (
     <div id="hello">
       {/*  when it's time to draft */}
-      <p>
+      {/* <p>
         The draft is open!
         <br />
         <br /> The next tournament begins on September 14th, so make sure you draft before then!
@@ -14,10 +14,10 @@ function Hello() {
         If you've created multiple accounts / teams and
         you want to consolidate, or if you notice any other unusual site
         behavior, feel free to email me - mtfujisumo[at]gmail[dot]com
-      </p>
+      </p> */}
 
       {/* when the tournament is live */}
-      {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p>  */}
+      <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p> 
 
       {/* if you want to let someone contact you for some reason */}
       {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo at gmail.com with your team name and new pick.)</p> */}
