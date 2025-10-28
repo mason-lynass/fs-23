9153d14b55fdacdc670395b751ac4416b3909ee3
Mason Lynass <masonlynass@gmail.com>
2025-10-27 23:02:15 -0700

    backend stuff

--- app/serializers/rikishi_serializer.rb
+++ app/serializers/rikishi_serializer.rb
@@ -1,3 +1,3 @@
 class RikishiSerializer < ActiveModel::Serializer
-  attributes :id, :shikona, :image_url, :birthdate, :highest_rank, :current_rank, :heya, :height, :weight, :yusho, :kinboshi, :shukun_sho, :gino_sho, :kanto_sho, :fs_current, :retired
+  attributes :id, :shikona, :image_url, :birthdate, :highest_rank, :current_rank, :heya, :height, :weight, :yusho, :kinboshi, :shukun_sho, :gino_sho, :kanto_sho, :fs_current, :retired, :b202509
 end
