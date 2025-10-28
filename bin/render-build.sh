7bf733f8bc3b145c9af8b5e4a5c454d903ec4d54
Mason Lynass <masonlynass@gmail.com>
2025-10-28 14:25:52 -0700

    doing Render again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# builds the front end code
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
+# Skip frontend build - frontend hosted elsewhere
+# rm -rf public
+# npm install --prefix client && npm run build --prefix client
+# cp -a client/build/. public/
 
 # builds the back end code
 bundle install
