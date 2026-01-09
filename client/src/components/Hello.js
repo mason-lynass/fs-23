d829323ce6d948c3e60689bda081032e5f991be5
Mason Lynass <masonlynass@gmail.com>
2026-01-09 14:59:28 -0800

    tip-jar

--- client/src/components/Hello.js
+++ client/src/components/Hello.js
@@ -2,7 +2,7 @@ function Hello() {
   return (
     <div id="hello">
       {/*  when it's time to draft */}
-      <p>
+      {/* <p>
         The draft is open!
         <br />
         <br /> The next tournament begins on January 11th, so make sure you
@@ -13,10 +13,12 @@ function Hello() {
         If you've created multiple accounts / teams and you want to consolidate,
         or if you notice any other unusual site behavior, feel free to email me
         - mtfujisumo[at]gmail[dot]com
-      </p>
+      </p> */}
 
       {/* when the tournament is live */}
-   {/* <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p> */}
+   <p>The tournament has started, and the draft is closed! The next draft will begin after the banzuke is announced for the next tournament.</p>
+   <br />
+   <p id='tip-jar'>I added a Ko-fi tip jar - tips offset server & hosting costs for the site. This site will always be free to play, but any donation would make my day :) </p>
 
       {/* if you want to let someone contact you for some reason */}
       {/* <p>(If you picked someone that's out of the tournament, feel free to email me at mtfujisumo[at]gmail[dot]com with your team name and new pick.)</p> */}
